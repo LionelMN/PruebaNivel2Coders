09e13cf2f914cb4e332676386566180775992564
Lionel Montesdeoca <lionelmontesdeocanuez.com>
2022-10-13 12:09:57 +0100

    MovieDetails View done, fixed moviesI and updtated moviesService

--- src/app/interfaces/movie-i.ts
+++ src/app/interfaces/movie-i.ts
@@ -1,16 +1,42 @@
 export interface MovieI {
   "adult": boolean,
   "backdrop_path": string,
-  "id": number,
-  "title": string,
+  "belongs_to_collection": {
+    "id" : string,
+    "name" : string,
+    "poster_path" : string,
+    "backdrop_path" : string,
+  },
+  "budget": number,
+  "genres": {
+    "id": string,
+    "name": string
+  }[],
+  "homepage": string,
+  "id": string,
+  "imdb_id": string,
   "original_language": string,
   "original_title": string,
   "overview": string,
-  "poster_path": string,
-  "media_type": string,
-  "genre_ids": [number],
   "popularity": number,
+  "poster_path": string,
+  "production_companies": {
+    "id": string,
+    "logo_path": string,
+    "name": string,
+    "origin_country": string
+  }[],
+  "production_countries": {
+    "iso_3166_1": string,
+    "name": string
+  }[],
   "release_date": Date,
+  "revenue": number,
+  "runtime": number,
+  "spoken_languages": [],
+  "status": string,
+  "tagline": string,
+  "title": string,
   "video": boolean,
   "vote_average": number,
   "vote_count": number
--- src/app/views/movie-details/movie-details.component.ts
+++ src/app/views/movie-details/movie-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { MovieI } from '../../interfaces/movie-i';
+import { MoviesService } from '../../services/movies/movies.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private moviesService : MoviesService,
+    private route : ActivatedRoute
+  ) { }
+
+  private movieId : string | null= '';
+  public movie? : MovieI;
 
   ngOnInit(): void {
+    this.movieId =  this.route.snapshot.paramMap.get('id');
+    this.getMovie(this.movieId);
+  }
+
+  private getMovie(id : string | null) : void{
+    this.moviesService.getMovieById(id).subscribe( m => {
+      console.log(m);
+
+      this.movie = m;
+    })
   }
 
 }
